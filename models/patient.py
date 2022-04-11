464d043082372905b909b8b083941e703df5db4d
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-04-12 00:34:50 +0200

    status bar and some controlers

--- models/patient.py
+++ models/patient.py
@@ -8,10 +8,30 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient Model"
 
     name = fields.Char(string='Name', required=True)
-    age = fields.Integer (string='Age')
+    age = fields.Integer(string='Age')
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other'),
-    ], required=True, default='other')
+    ], required=True)
     note = fields.Text(string='Description')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirmed'),
+        ('done', 'Done'),
+        ('cancel', 'cancelled'),
+    ], default='draft', string="Status")
+
+    def action_confirm(self):
+        if self.state != 'cancel':
+            self.state = 'cancel'
+        else:
+            print("not allowed")
+
+    def action_done(self):
+        if self.state != 'done':
+            self.state = 'done'
+        else:
+            print("not allowed")
+
+
