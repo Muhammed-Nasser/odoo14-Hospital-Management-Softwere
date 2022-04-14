319e2101e34ae82cb3f84b3cae532c07f98f95a0
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-04-14 21:21:49 +0200

    add onchange method

--- models/appointments.py
+++ models/appointments.py
@@ -14,12 +14,19 @@ class HospitalAppointments(models.Model):
         ('done', 'Done'),
         ('cancel', 'cancelled'),
     ], default='draft', string="Status", tracking=True)
+    # to test onchange method
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ], required=True, tracking=True)
     # Many2one Relation
     patient_id = fields.Many2one('hospital.patient', string="Patient", required=True, tracking=True)
     reference = fields.Char(string='appointment Reference', required=True, copy=False, readonly=True,
                             default=lambda self: _('New'))
     date_appointment = fields.Date(string="Date")
     date_check = fields.Datetime(string="Check Up Time")
+    # related fields
     age = fields.Integer(string='Age', related='patient_id.age')
     responsible_id = fields.Many2one('res.partner', string="Responsible", tracking=True,
                                      related='patient_id.responsible_id')
@@ -36,6 +43,7 @@ class HospitalAppointments(models.Model):
         else:
             print("not allowed")
 
+    # override create function
     @api.model
     def create(self, vals):
         if not vals.get('note'):
@@ -43,6 +51,20 @@ class HospitalAppointments(models.Model):
         if vals.get('reference', _('New')) == _('New'):
             vals['reference'] = self.env['ir.sequence'].next_by_code('appointment.no') or _('New')
         return super(HospitalAppointments, self).create(vals)
+
+    # onchange function
+    @api.onchange('patient_id', 'note')
+    def onchange_patient_id(self):
+        if self.patient_id:
+            # check if there is a value in patient_id.gender => gender = patient_id.gender
+            if self.patient_id.gender:
+                self.gender = self.patient_id.gender
+            # check if there is a value in patient_id.note => note = patient_id.note
+            if self.patient_id.note:
+                self.note = self.patient_id.note
+        else:
+            self.gender = ''
+            self.note = ''
 
 
 
